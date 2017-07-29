b615c1dfa01c61eb7798291472ea58803d445270
Avi Jassra <avijassra@hotmail.com>
2017-07-29 11:58:41 -0400

    c# problem 1

--- Problem001/CSharp/Program.cs
+++ Problem001/CSharp/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace CSharp
+﻿namespace CSharp
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stopWatch = Stopwatch.StartNew();
+            stopWatch.Start();
+
+            var sum = 0;
+
+            for(var i = 1; i < 1000; i++) {
+                if(i%3 == 0 || i%5 == 0) {
+                    sum += i;
+                }   
+            }
+            stopWatch.Stop();
+
+            Console.WriteLine("Total {0} (time: {1}ms)", sum, stopWatch.Elapsed.TotalMilliseconds);
+            
         }
     }
 }
