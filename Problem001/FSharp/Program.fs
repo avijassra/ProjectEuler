5d1ecb8ea066ba30d17cc4df258a1e59be67f581
Avi Jassra <avijassra@hotmail.com>
2017-07-29 07:00:21 -0400

    Problem 1 solved in F#

--- Problem001/FSharp/Program.fs
+++ Problem001/FSharp/Program.fs
@@ -4,5 +4,8 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    [|1..999|]
+        |> Array.filter (fun x -> x % 3 = 0 || x % 5 = 0)
+        |> Array.sum
+        |> printfn "Total %d" 
     0 // return an integer exit code
