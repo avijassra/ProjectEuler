a0a7a221aac9bf79ddd638a4c36a0d3594b31dfa
Avi Jassra <avijassra@hotmail.com>
2017-07-29 08:46:39 -0400

    added stopwatch

--- Problem001/FSharp/Program.fs
+++ Problem001/FSharp/Program.fs
@@ -4,8 +4,13 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    [|1..999|]
-        |> Array.filter (fun x -> x % 3 = 0 || x % 5 = 0)
-        |> Array.sum
-        |> printfn "Total %d" 
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+
+    let sum = [|1..999|]
+                |> Array.filter (fun x -> x % 3 = 0 || x % 5 = 0)
+                |> Array.sum
+    
+    stopWatch.Stop()
+
+    printfn "Total %d (time %fms)" sum stopWatch.Elapsed.TotalMilliseconds 
     0 // return an integer exit code
