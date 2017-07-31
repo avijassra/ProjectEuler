0b3b68e40386ec118283df7df4ff85eef51cb271
Avi Jassra <avijassra@hotmail.com>
2017-07-31 05:40:50 -0400

    updated

---
+++ Problem0002/FSharp/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let fabSeq = Seq.unfold (fun(a,b) -> Some (a+b (b, a+b)) ) (0, 1)
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
