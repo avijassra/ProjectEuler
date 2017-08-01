68e72f52b08fa6d5d033e25b98a5a00200bfc95f
Avi Jassra <avijassra@hotmail.com>
2017-08-01 09:06:45 -0400

    Problem 002 fixed

--- Problem0002/FSharp/Program.fs
+++ Problem0002/FSharp/Program.fs
@@ -4,7 +4,16 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    let fabSeq = Seq.unfold (fun(a,b) -> Some (a+b (b, a+b)) ) (0, 1)
-    
-    printfn "Hello World from F#!"
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+
+    Seq.unfold (fun (a,b) -> Some(a+b, (b, a+b))) (0, 1)
+        |> Seq.takeWhile (fun x -> x < 4000000)
+        |> Seq.toList
+        |> Seq.filter (fun x-> x%2 = 0)
+        |> Seq.sum
+        |> printfn "Total %d"
+
+    stopwatch.Stop()
+
+    printfn "Time: %fms" stopwatch.Elapsed.TotalMilliseconds
     0 // return an integer exit code
