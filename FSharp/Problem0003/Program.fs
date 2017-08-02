e2af399363153ac8a8e67301a313015ec05f4463
Avi Jassra <avijassra@hotmail.com>
2017-08-02 13:06:30 -0400

    solved Problem 3

--- FSharp/Problem0003/Program.fs
+++ FSharp/Problem0003/Program.fs
@@ -5,7 +5,16 @@ open System
 [<EntryPoint>]
 let main argv =
     let stopwatch = System.Diagnostics.Stopwatch.StartNew()
-    let bigNumber: BigInteger = 600851475143
-    Seq { for i in 2 .. bigNumber/2 -> i}
-        |> Seq.iter ( printfn "%d " )
+    
+    let rec CalculatePrimeFactor  (num:int64) (lpf:int64) =
+        match num with
+            | _ when float lpf > Math.Sqrt(float num) -> num
+            | _ when num%lpf = 0L -> CalculatePrimeFactor (num/lpf) 2L
+            | _ -> CalculatePrimeFactor num (lpf+1L)
+
+    let largestPrimeFactor = CalculatePrimeFactor 600851475143L 2L
+
+    stopwatch.Stop()
+
+    printfn "Largest Prime Factor: %d (time: %fms)" largestPrimeFactor stopwatch.Elapsed.TotalMilliseconds
     0 // return an integer exit code
