34b34c28665e0c7e76964ab14d70904f8b6f60bf
Avi Jassra <avijassra@hotmail.com>
2017-08-01 21:26:23 -0400

    f# problem 3 project created

---
+++ FSharp/Problem0003/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+    let bigNumber: BigInteger = 600851475143
+    Seq { for i in 2 .. bigNumber/2 -> i}
+        |> Seq.iter ( printfn "%d " )
+    0 // return an integer exit code
