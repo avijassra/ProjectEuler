c6e8ef5f39b5eb6dcefc25469d9dc3a969f908e2
Avi Jassra <avijassra@hotmail.com>
2017-08-04 15:25:50 -0400

    WIP

---
+++ CSharp/Problem0015/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem0015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var gridLimit = 3;
+
+            stopwatch.Stop();
+
+        }
+
+        public int GenerateAllRouteOptionsInGrid(int gridLimit) {
+            if()
+
+            return 0;
+        }
+        
+        public int[,] FindNewRouteOptionsInGrid(List<Tuple<int, int>> gridRoute) {
+            return null;
+        }
+    }
+}
