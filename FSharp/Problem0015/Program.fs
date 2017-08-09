42fccbf7a6c93141c141cad874a17ad5347e099d
Avi Jassra <avijassra@hotmail.com>
2017-08-09 18:22:52 -0400

    WIP

---
+++ FSharp/Problem0015/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let sqLimit = 3
+    let sqStartingPoint = (0,0)
+
+    
+
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
