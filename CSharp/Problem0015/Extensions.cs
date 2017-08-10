395408d17034fa3f1026c995b2dcc6a80fbf7218
Avi Jassra <avijassra@hotmail.com>
2017-08-09 20:58:02 -0400

    updated code

--- CSharp/Problem0015/Extensions.cs
+++ CSharp/Problem0015/Extensions.cs
@@ -5,23 +5,27 @@ namespace System.Collections.Generic
 
     public static class ListExtension
     {
-        public static List<Tuple<int, int>> MoveOnX(this List<Tuple<int, int>> routeOption) 
+        public static List<Tuple<int, int>> MoveOnX(this List<Tuple<int, int>> routeOption, int maxX) 
         {
             var newRouteOption = routeOption.Select(x => new Tuple<int, int>(x.Item1, x.Item2)).ToList();
             var lastPoint = newRouteOption.Last();
 
-            newRouteOption.Add(new Tuple<int, int>(lastPoint.Item1 + 1, lastPoint.Item2));
-
+            if(lastPoint.Item1 < maxX) {
+                newRouteOption.Add(new Tuple<int, int>(lastPoint.Item1 + 1, lastPoint.Item2));
+            }
+            
             return newRouteOption;
         }
 
-        public static List<Tuple<int, int>> MoveOnY(this List<Tuple<int, int>> routeOption) 
+        public static List<Tuple<int, int>> MoveOnY(this List<Tuple<int, int>> routeOption, int maxY) 
         {
             var newRouteOption = routeOption.Select(x => new Tuple<int, int>(x.Item1, x.Item2)).ToList();
             var lastPoint = newRouteOption.Last();
 
-            newRouteOption.Add(new Tuple<int, int>(lastPoint.Item1, lastPoint.Item2 + 1));
-
+            if(lastPoint.Item2 < maxY) {
+                newRouteOption.Add(new Tuple<int, int>(lastPoint.Item1, lastPoint.Item2 + 1));
+            }
+            
             return newRouteOption;
         }
     }
--- CSharp/Problem0015/Program.cs
+++ CSharp/Problem0015/Program.cs
@@ -9,7 +9,8 @@
         static void Main(string[] args)
         {
             var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-            var gridLimit = 3;
+            var gridLimit = 2;
+            var allOptionsNotExplored = true;
 
             var routeOptions = new List<List<Tuple<int, int>>>();
             var startPoint = new List<Tuple<int, int>>() {
@@ -18,20 +19,37 @@
 
             routeOptions.Add(startPoint);
 
-            for(var ind = 0; ind < gridLimit; ind++) {
+            while(allOptionsNotExplored) {
                 var copyOfRouteOptions = routeOptions.Select(x => x).ToList();
+                routeOptions = new List<List<Tuple<int, int>>>();
                 
-                foreach(var opt in routeOptions) {
-                    var optX = opt.MoveOnX();
-                    var optY = opt.MoveOnY();
-                }
+                foreach(var opt in copyOfRouteOptions) {
+                    var newMoveOnX = opt.MoveOnX(gridLimit);
+                    routeOptions.Add(newMoveOnX);
+                    var newMoveOnY = opt.MoveOnY(gridLimit);
+                    routeOptions.Add(newMoveOnY);
 
-                
+                    var xLastPoint = newMoveOnX.Last();
+                    var yLastPoint = newMoveOnY.Last();
+
+                    allOptionsNotExplored = xLastPoint.Item1 < gridLimit || 
+                                                xLastPoint.Item2 < gridLimit || 
+                                                yLastPoint.Item1 < gridLimit || 
+                                                yLastPoint.Item2 < gridLimit;
+                    Console.WriteLine($"allOptionsNotExplored: {allOptionsNotExplored}");
+
+                    Console.WriteLine(string.Join(" -> ", newMoveOnX.Select(x => string.Format($"({x.Item1}, {x.Item2})"))));
+                    Console.WriteLine(string.Join(" -> ", newMoveOnY.Select(x => string.Format($"({x.Item1}, {x.Item2})"))));
+                }
             }
 
+            foreach(var opt in routeOptions) {
+                Console.WriteLine(string.Join(" -> ", opt.Select(x => string.Format($"({x.Item1}, {x.Item2})"))));
+            }
 
             stopwatch.Stop();
 
+            Console.WriteLine($"Total options: {routeOptions.Count} (time {stopwatch.Elapsed.TotalMilliseconds}ms)");
         }
     }
 }
