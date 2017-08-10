cc9a1129a439953b0c421e13c7d5d7522644a919
Avi Jassra <avijassra@hotmail.com>
2017-08-10 15:23:12 -0400

    WIP

--- CSharp/Problem0015/Program.cs
+++ CSharp/Problem0015/Program.cs
@@ -9,60 +9,20 @@
         static void Main(string[] args)
         {
             var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-            var gridSize = 14;
-            var gridLimit = gridSize - 1;
-            var allOptionsNotExplored = true;
+            const int gridSize = 3;
 
-            var routeOptions = new List<List<Tuple<int, int>>>();
-            var startPoint = new List<Tuple<int, int>>() {
-                new Tuple<int, int> (0,0)
-            };
+            var grid = new long[gridSize + 1, gridSize + 1];
 
-            routeOptions.Add(startPoint);
-
-            while(allOptionsNotExplored)
-            {
-                //Console.WriteLine("***********************************************************");
-                var copyOfRouteOptions = routeOptions.Select(x => x).ToList();
-                routeOptions = new List<List<Tuple<int, int>>>();
-
-                foreach (var opt in copyOfRouteOptions)
-                {
-                    var newMoveOnX = opt.MoveOnX(gridLimit);
-                    if(null != newMoveOnX) {
-                        routeOptions.Add(newMoveOnX);
-                    }
-                    
-                    var newMoveOnY = opt.MoveOnY(gridLimit);
-                    if(null != newMoveOnY) {
-                        routeOptions.Add(newMoveOnY);
-                    }
-
-                    var xLastPoint = newMoveOnX?.Last();
-                    var yLastPoint = newMoveOnY?.Last();
-
-                    allOptionsNotExplored = xLastPoint?.Item1 < gridLimit ||
-                                                xLastPoint?.Item2 < gridLimit ||
-                                                yLastPoint?.Item1 < gridLimit ||
-                                                yLastPoint?.Item2 < gridLimit;
-                    //Console.WriteLine($"allOptionsNotExplored: {allOptionsNotExplored}");
-
-                    // if(null != newMoveOnX) {
-                    //     Console.WriteLine(string.Join(" -> ", newMoveOnX.Select(x => string.Format($"({x.Item1}, {x.Item2})"))));
-                    // }
-                    // if(null != newMoveOnY) {
-                    //     Console.WriteLine(string.Join(" -> ", newMoveOnY.Select(x => string.Format($"({x.Item1}, {x.Item2})"))));
-                    // }
-                }
+            for(var i = 0; i < gridSize; i++) {
+                grid[i, gridSize] = 1;
+                grid[gridSize, 1] = 1;
             }
 
-            // foreach (var opt in routeOptions) {
-            //     Console.WriteLine(string.Join(" -> ", opt.Select(x => string.Format($"({x.Item1}, {x.Item2})"))));
-            // }
+            //for(var i = gridSize - 1)
 
             stopwatch.Stop();
 
-            Console.WriteLine($"Total options: {routeOptions.Count} (time {stopwatch.Elapsed.TotalMilliseconds}ms)");
+            //Console.WriteLine($"Total options: {routeOptions.Count} (time {stopwatch.Elapsed.TotalMilliseconds}ms)");
         }
     }
 }
