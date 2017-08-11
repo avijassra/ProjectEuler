3fcb966074f52db0c02eab2b1e791987b75d3bbc
Avi Jassra <avijassra@hotmail.com>
2017-08-11 05:42:39 -0400

    WIP

--- FSharp/Problem0015/Program.fs
+++ FSharp/Problem0015/Program.fs
@@ -4,10 +4,15 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    let sqLimit = 3
-    let sqStartingPoint = (0,0)
+    let gridSize = 2
+    let oneArray = Array.create gridSize+1 nan
+    let grid = Array.create gridSize+1 oneArray
+    grid.[3].[3] <- 1.0
 
-    
+    for i in 0 .. gridSize do
+    for j in 0 .. gridSize do
+        printfn "grid[%d][%d] = %d" i j grid.[i].[j]
 
-    printfn "Hello World from F#!"
+
+    //printfn "Hello World from F#!"
     0 // return an integer exit code
