6c8bfbcb3a228829d4650eddff84516c165b8224
Avi Jassra <avijassra@hotmail.com>
2017-08-16 18:51:41 -0400

    updated TS p15

--- TypeScript/Problem00015/main.ts
+++ TypeScript/Problem00015/main.ts
@@ -1,25 +1,35 @@
-import * as _ from "lodash";
-
 class Main {
+    private gridSize = 20;
+    private grid: number[][] = [];
+        
 
     solve(): void {
         console.time("Problem 15")
 
-        let gridSize = 2;
-        let grid: number[][];
+        for(var i= 0; i < this.gridSize +1; i++) {
+            if(!this.grid) {
+                this.grid = [];
+            }
 
-        for(var i= 0; i < gridSize +1; i++) {
-            grid[i][gridSize] = 1;
-            grid[gridSize][i] = 1;
+            if(!this.grid[i]) {
+                this.grid[i] = [];
+            }
+
+            if(!this.grid[this.gridSize]) {
+                this.grid[this.gridSize] = [];
+            }
+
+            this.grid[i][this.gridSize] = 1;
+            this.grid[this.gridSize][i] = 1;
         }
 
-        for(var i = gridSize - 1; i >= 0; i--) {
-            for(var j = gridSize - 1; j >= 0; j--) {
-                grid[i][j] = grid[i][j+1] + grid[i+1][j]
+        for(var i = this.gridSize - 1; i >= 0; i--) {
+            for(var j = this.gridSize - 1; j >= 0; j--) {
+                this.grid[i][j] = this.grid[i][j+1] + this.grid[i+1][j]
             }
         }
 
-        console.log(`Route options: ${grid[0][0]}`);
+        console.log(`Route options: ${this.grid[0][0]}`);
         console.timeEnd("Problem 15");
     }
 }
